83e4e286487e812edab617dbb8a6a27e7e0bbdc9
Rodgers Chayuga <chayugarodgers@gmail.com>
2024-12-20 16:22:39 +0300

    frontend: build UI structure, image and progress bar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,58 +1,42 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import ProgressBar from "@/components/progress-bar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="grid dark:bg-gray-800 bg-[#F5F5FF] grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
+      <ModeToggle />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex gap-8 row-start-2 items-center sm:items-start ">
+        <div className="flex-1">
+          <>
+            <h3 className="text-5xl font-extrabold">Help Children Reach</h3>
+            <h3 className="text-5xl font-extrabold">Their Full Potential</h3>
+          </>
+          <p>
+            We work tirelessly to ensure that children have access to quality education, regardless of their background. Our initiatives range from building schools in underserved communities to providing scholarships and educational materials.
+          </p>
+
         </div>
+        <div className="flex-1" >
+          <div className="h-[500px] relative">
+            <Image
+              src="/images/student.jpg"
+              alt="Picture of the students in school"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+          <div>
+            <ProgressBar />
+          </div>
+        </div>
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center ">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://x.com/BlockchainDocta"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -63,11 +47,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          X (Twitter)
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/RodgersChayuga/Charity-Donation-Platform"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -78,11 +62,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          GitHub Repo
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://rodgerschayuga.com"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -93,7 +77,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Portfolio site →
         </a>
       </footer>
     </div>
